905eef8a1f4b86c0aefe7e95535c667281e5e637
militsa_toncheva <mili.toncheva@icloud.com>
2023-01-16 14:21:05 +0200

    Add project files.

---
+++ Scrabble/Scrabble.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+void Exit()
+{
+
+}
+void AddNewWord()
+{
+
+}
+void Settings()
+{
+
+}
+void NewGame()
+{
+
+}
+void Menu()
+{
+    cout << "Menu:" << endl;
+    cout << "1. Start new game" << endl;
+    cout << "2. Settings" << endl;
+    cout << "3. Add new word" << endl;
+    cout << "4. Exit" << endl;
+    int choice;
+    cin >> choice;
+    switch (choice)
+    {
+    case 1: NewGame();
+        break;
+    case 2: Settings();
+        break;
+    case 3: AddNewWord();
+        break;
+    case 4: Exit();
+        break;
+
+    default:
+        break;
+    }
+}
+int main()
+{
+    Menu();
+}
